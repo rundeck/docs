1236d8d22d27da1e19a5d6931d09d81feba528d4
Greg Schueler <greg.schueler@gmail.com>
2018-11-21 12:43:39 -0800

    Add expandable plugin to search box for improved autocomplete

--- en/docs-autocomplete.js
+++ en/docs-autocomplete.js
@@ -13,6 +13,61 @@ function setupAutocomplete(sitedata){
 		jQuery('.navquerytoggle').collapse('toggle');
 	});
 }
+/*!
+ * jQuery Expandable Input Plugin v1.0
+ * https://github.com/armmer1/expandable-input
+ *
+ * Copyright 2014, Nattawat Nonsung
+ */
+
+
+/**
+* Use immediately Invoked Function Expression to
+* - Prevent conflicting with other libary on alias $
+* - Scope varaible to be private
+*/
+(function( $ ) {
+  $.fn.expandable = function(options) {
+
+		// Define default setting
+		var settings = $.extend({
+			width: 150,
+			duration: 300
+    }, options );
+
+		var width = this.outerWidth();
+
+		this.on("focus", function(){
+			// $(this).css({width: settings.width});
+			$(this).animate({
+	      width: settings.width
+	    }, settings.duration, function(){
+	      // callback function
+	    });
+		});
+
+		this.on("blur", function(){
+			$(this).animate({
+	      width: width
+	    }, settings.duration, function(){
+	      // callback function
+	    });
+		});
+
+		if(settings.action && typeof(settings.action) == "function"){
+			this.on('keypress', function(e) {
+				if (e.which === 13) { // press enter
+					settings.action($(this).val());
+				}
+			});
+		}
+		// Return jQuery so that it's chainable 
+		return this;		
+  };
+ 
+}( jQuery ));
+
+
 jQuery(function(){
 	
 
@@ -20,6 +75,11 @@ jQuery(function(){
 		var site=data;
 		setupAutocomplete(site);		
 	});
+	$("#searchQuery").expandable({
+	    		width: 500,
+	    		duration: 300
+	    	});
+	
   $('[data-toggle="tooltip"]').tooltip();
   $('[data-has-toggle="tooltip"]').tooltip();
  
