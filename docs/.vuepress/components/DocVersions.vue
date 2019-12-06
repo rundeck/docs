102c103c0b2cfdc282af2d3587245b5306383ab6
Greg Schueler <greg.schueler@gmail.com>
2019-12-05 18:59:28 -0800

    Add previous docs version page and links from readme

---
+++ docs/.vuepress/components/DocVersions.vue
@@ -0,0 +1,61 @@
+<template>
+	
+	<label>
+	Version:
+
+		<select onchange="if(this.value){document.location=this.value}">
+			<option value="" >-Select-</option>
+			<optgroup :label="group.label" v-for="group in versions" :key="group.label">
+			<option v-for="vers in group.values" :value="docsbase+'/'+vers" :key="vers">{{vers}}</option>
+			</optgroup>
+		</select>
+		        
+            
+            </select>
+	</label>
+</template>
+
+<script>
+const docsBase= "https://docs.rundeck.com"
+//include the last version in each minor release
+const previousDocsVersions= [
+	"3.2.0",
+	"3.1.3",
+	"3.0.27",
+	"2.11.14",
+	"2.10.8",
+	"2.9.2",
+	"2.8.4",
+	"2.7.3",
+	"2.6.11",
+	"2.5.3",
+	"2.4.2",
+	"2.3.2",
+	"2.2.3",
+	"2.1.3",
+	"2.0.4"
+]
+let versarr=[]
+//parse versions to create groups of options
+for(let a in previousDocsVersions){
+	let b = previousDocsVersions[a]
+	let match=b.match(/^(\d+\.\d+)\.(\d+)$/)
+	let minor=match[1]
+	let count=parseInt(match[2])
+	let strings=[]
+	for(let i=count;i>=0;i--){
+		strings.push(minor+`.${i}`)
+	}
+	versarr.push({label:minor,values:strings})
+	
+}
+export default {
+	data(){
+		return {
+			versions: versarr,
+			docsbase: docsBase
+		}
+	},
+
+}
+</script>
---
+++ docs/manual/old-docs.md
@@ -0,0 +1,16 @@
+---
+sidebar: false
+---
+
+# Older Documentation Versions
+
+<DocVersions/>
+
+## Archives
+
+Documentation source for all versions
+
+:   <https://github.com/rundeck/docs/tags>
+
+Generated HTML (up to version 3.1.0)
+:    <https://github.com/rundeck/rundeck-docs/branches/all>
