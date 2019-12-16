502820df6ccad0f5d4418f8b88e127fe84dd44cc
Greg Schueler <greg.schueler@gmail.com>
2019-12-16 12:24:02 -0800

    General upgrade guide updated to rundeck 3
    remove rundeck 2 content

--- docs/.vuepress/sidebar-menus/upgrading.js
+++ docs/.vuepress/sidebar-menus/upgrading.js
@@ -3,9 +3,10 @@ module.exports = [{
   collapsable: false,
   sidebarDepth: 1,
   children: [
+    '/upgrading/',
     '/upgrading/upgrading',
-    '/upgrading/upgrading-to-rundeck3',
+    '/upgrading/upgrading-to-rundeck-3.2.md',
     '/upgrading/upgrading-to-rundeck-3.1.md',
-    '/upgrading/upgrading-to-rundeck-3.2.md'
+    '/upgrading/upgrading-to-rundeck3',
   ]
 }]
