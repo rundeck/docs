ebf4d738bfba3960362f0bd6d6a35f2107a0ef0d
Forrest Evans <fdevans@gmail.com>
2020-06-03 11:26:30 -0700

    Webhooks Collapsable
    Made Webhooks collapsable like other menu items.

--- docs/.vuepress/sidebar-menus/user-guide.js
+++ docs/.vuepress/sidebar-menus/user-guide.js
@@ -38,7 +38,7 @@ module.exports = [{
     '/manual/11-node-filters.md',
     {
       title: 'Webhooks',
-      collapsable: false,
+      collapsable: true,
       sidebarDepth: 2,
       children: [
         '/manual/12-webhooks.md',
