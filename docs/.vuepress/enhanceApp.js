e8512af8b9d2f7478c23577ffecb00a401d54cbf
ProTip <greg.zapp@gmail.com>
2020-06-02 20:54:14 -0500

    Update service workers on route change

---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,16 @@
+export default ({
+    Vue, // the version of Vue being used in the VuePress app
+    options, // the options for the root Vue instance
+    router, // the router instance for the app
+    siteData, // site metadata
+    isServer // is this enhancement applied in server-rendering or client
+}) => {
+  /** Update service workers on navigation */
+  router.afterEach( (to, from) => {
+      navigator.serviceWorker.getRegistrations().then( regos => {
+          regos.forEach(r => {
+              r.update()
+          })
+      })
+  })
+}
