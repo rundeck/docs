db48ac115ed9afe1ff4f228b28aa9ff36ef77349
mishingo <mr@mishingo.com>
2020-08-04 14:17:56 -0400

    re-write analytics js loader

--- docs/.vuepress/public/js/gtm.js
+++ docs/.vuepress/public/js/gtm.js
@@ -1,3 +1,21 @@
-let gtmScript = document.createElement('noscript')
-gtmScript.innerHTML = '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M2STD5F" height="0" width="0" style="display:none;visibility:hidden"></iframe>'
-document.body.appendChild(gtmScript)
+/**
+ * Documentation Analytics
+ **/
+
+// Google Tag Manager
+
+let gtmNoScript = document.createElement('noscript')
+let gtmIframe = document.createElement('iframe')
+gtmIframe.src = 'https://www.googletagmanager.com/ns.html?id=GTM-M2STD5F'
+gtmIframe.style.display = 'none'
+gtmNoScript.appendChild(gtmIframe)
+document.body.appendChild(gtmNoScript)
+
+// Lead Forensics
+
+let leadForensicsNoScript = document.createElement('noscript')
+let leadForensicsImg = document.createElement('img')
+leadForensicsImg.src = 'https://secure.peak2poem.com/195720.png?trk_user=195720&trk_tit=jsdisabled&trk_ref=jsdisabled&trk_loc=jsdisabled'
+leadForensicsImg.style.display = 'none'
+leadForensicsNoScript.appendChild(leadForensicsImg)
+document.body.appendChild(leadForensicsNoScript)
