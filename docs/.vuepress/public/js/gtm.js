5073a95010f26dc930806fb4052d3ebc78c22d6c
Miguel Ramos <mr@mishingo.com>
2020-08-14 00:06:46 -0400

    Add Lead Feeder Analytics

--- docs/.vuepress/public/js/gtm.js
+++ docs/.vuepress/public/js/gtm.js
@@ -19,3 +19,11 @@ leadForensicsImg.src = 'https://secure.peak2poem.com/195720.png?trk_user=195720&
 leadForensicsImg.style.display = 'none'
 leadForensicsNoScript.appendChild(leadForensicsImg)
 document.body.appendChild(leadForensicsNoScript)
+
+// Lead Feeder
+
+let leadFeederScript = document.createElement('script')
+var inlineScript = document.createTextNode("(function(){ window.ldfdr = window.ldfdr || {}; (function(d, s, ss, fs){ fs = d.getElementsByTagName(s)[0]; function ce(src){ var cs = d.createElement(s); cs.src = src; setTimeout(function(){fs.parentNode.insertBefore(cs,fs)}, 1); } ce(ss); })(document, 'script', 'https://sc.lfeeder.com/lftracker_v1_bElvO73eWP1aZMqj.js'); })(); ");
+leadFeederScript.appendChild(inlineScript); 
+document.body.appendChild(leadFeederScript);
+
