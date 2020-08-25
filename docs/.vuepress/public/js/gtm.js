7b125c942fafd34ed8ef5681a116ba9a5d6861cc
Miguel Ramos <mr@mishingo.com>
2020-08-25 14:04:42 -0400

    Add HotJar

--- docs/.vuepress/public/js/gtm.js
+++ docs/.vuepress/public/js/gtm.js
@@ -27,3 +27,12 @@ var inlineScript = document.createTextNode("(function(){ window.ldfdr = window.l
 leadFeederScript.appendChild(inlineScript); 
 document.body.appendChild(leadFeederScript);
 
+// HotJar
+
+let hotJarScript = document.createElement('script')
+var hotJarInlineScript = document.createTextNode("(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:1960784,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv='); ");
+hotJarScript.appendChild(hotJarInlineScript); 
+document.body.appendChild(hotJarScript);
+
+
+    
