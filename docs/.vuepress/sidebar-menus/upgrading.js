604c11ba6e87adae7f67222edb04f317d1867936
Forrest Evans <fdevans@gmail.com>
2021-08-30 16:39:30 -0700

    fixes #875
    fixes #875

--- docs/.vuepress/sidebar-menus/upgrading.js
+++ docs/.vuepress/sidebar-menus/upgrading.js
@@ -7,7 +7,6 @@ module.exports = [{
     '/upgrading/upgrading',
     '/upgrading/upgrading-to-rundeck-3.4.md',
     '/upgrading/upgrading-to-rundeck-3.3.4.md',
-    '/upgrading/upgrading-to-rundeck-3.3.4.md',
     '/upgrading/upgrading-to-rundeck-3.3.md',
     '/upgrading/upgrading-to-rundeck-3.2.md',
     '/upgrading/upgrading-to-rundeck-3.1.md',
