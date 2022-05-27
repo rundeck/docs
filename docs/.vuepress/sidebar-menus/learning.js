7e5485697b490adbad31eb1602895530b9b90c92
Jake Cohen <jnohec@gmail.com>
2022-05-26 20:27:23 -0700

    changing page labels

--- docs/.vuepress/sidebar-menus/learning.js
+++ docs/.vuepress/sidebar-menus/learning.js
@@ -112,13 +112,13 @@ module.exports = [{
           collapsable: true,
           sidebarDepth: 0,
           children: [
-            ['/learning/solutions/automated-diagnostics/solution-overview.md','Solution Summary'],
-            ['/learning/solutions/automated-diagnostics/getting-started.md', 'Getting Started'],
-            ['/learning/solutions/automated-diagnostics/tours.md', 'Following the Tours'],
-            ['/learning/solutions/automated-diagnostics/jobs.md', 'Prebuilt Jobs'],
-            ['/learning/solutions/automated-diagnostics/feedback-faq.md', 'Feedback & FAQ'],
-            ['/learning/solutions/auto-incident-kubernetes-logs.md','General Purpose: Kubernetes Logs'],
-            ['/learning/solutions/auto-diagnostics-github-script.md','General Purpose: Raw Scripts']
+            ['/learning/solutions/automated-diagnostics/solution-overview.md','1. Solution Summary'],
+            ['/learning/solutions/automated-diagnostics/getting-started.md', '2. Getting Started'],
+            ['/learning/solutions/automated-diagnostics/tours.md', '3. Following the Tours'],
+            ['/learning/solutions/automated-diagnostics/jobs.md', '4. Prebuilt Jobs'],
+            ['/learning/solutions/automated-diagnostics/feedback-faq.md', '5. Feedback & FAQ'],
+            ['/learning/solutions/auto-incident-kubernetes-logs.md','Example: Kubernetes Logs'],
+            ['/learning/solutions/auto-diagnostics-github-script.md','Example: Raw Scripts']
           ]
         }
       ]
