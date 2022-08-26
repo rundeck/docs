8dbec12edfa2cc5edab51413f1bc78e61768700f
Nathan Fluegel <wayfaringson@users.noreply.github.com>
2022-08-26 11:32:10 -0700

    Tweaks

--- docs/.vuepress/sidebar-menus/learning.js
+++ docs/.vuepress/sidebar-menus/learning.js
@@ -46,7 +46,7 @@ module.exports = [{
               '/learning/howto/use-terraform-provider.md',
               '/learning/howto/learn-rd-cli.md',
               '/learning/howto/acl_basic_examples.md',
-              '/learning/howto/workingwithlogs.md'
+              '/learning/howto/workinglogs.md'
           ]
         },
         {
