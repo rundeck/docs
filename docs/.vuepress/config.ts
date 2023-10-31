79b656c01488d75edf5685ef97c3ccf0709dcce6
Forrest Evans <fdevans@gmail.com>
2023-10-31 11:06:28 -0700

    GTM Codes

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -45,6 +45,14 @@ export default defineUserConfig({
   shouldPrefetch: false,
   base: `/${setup.base ? setup.base + '/' : ''}`,
   head: [
+    ['script', {}, `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-5QNBBN6');
+    `],
+    ['script', { src: '/js/gtm.js', defer: true }],
   ],
   extendsMarkdown: md => {
     md.use(markdownItReplaceVars, 'custom_token_replace', function (content) {
---
+++ docs/.vuepress/public/js/gtm.js
@@ -0,0 +1,12 @@
+/**
+ * Documentation Analytics
+ **/
+
+// Google Tag Manager
+
+let gtmNoScript = document.createElement('noscript')
+let gtmIframe = document.createElement('iframe')
+gtmIframe.src = 'https://www.googletagmanager.com/ns.html?id=GTM-5QNBBN6'
+gtmIframe.style.display = 'none'
+gtmNoScript.appendChild(gtmIframe)
+document.body.appendChild(gtmNoScript)
