fa4b1b3fabbb3d5f33d0bdf581b670e5887ce920
Forrest Evans <fdevans@gmail.com>
2023-11-16 09:09:32 -0800

    test nav build with different setup

--- docs/.vuepress/sidebar-menus/history.ts
+++ docs/.vuepress/sidebar-menus/history.ts
@@ -6,60 +6,52 @@ export default [
     text: 'Release Notes',
     collapsible: false,
     link: '/history/',
+    headerDepth: 1,
     children: [
       ...getHistory('docs/history/'),
       {
         text: "Security Advisories",
         collapsible: true,
-        link: '/history/cves/',
-        children: getChildren('docs/history/', 'cves'),
+        children: getHistory('docs/history/', 'cves'),
       },
       {
         text: '4.x',
         collapsible: true,
-        link: '/history/',
         children: getHistory('docs/history/', '4_x')
       },
       {
         text: '3.4.x',
         collapsible: true,
-        link: '/history/',
         children: getHistory('docs/history/', '3_4_x'),
       },
       {
         text: '3.3.x',
         collapsible: true,
-        link: '/history/',
         children: getHistory('docs/history/', '3_3_x'),
       },
       {
         text: '3.2.x',
         collapsible: true,
-        link: '/history/',
         children: getHistory('docs/history/', '3_2_x'),
       },
       {
         text: '3.1.x',
         collapsible: true,
-        link: '/history/',
         children: getHistory('docs/history/', '3_1_x'),
       },
       {
         text: '3.0.x',
         collapsible: true,
-        link: '/history/',
         children: getHistory('docs/history/', '3_0_x'),
       },
       {
         text: '2.x',
         collapsible: true,
-        link: '/history/',
         children: getHistory('docs/history/', '2_x'),
       },
       {
         text: '1.x',
         collapsible: true,
-        link: '/history/',
         children: getHistory('docs/history/', '1_x'),
       }
     ],
