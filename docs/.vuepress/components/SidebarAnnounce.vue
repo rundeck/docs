1c15f04b67ca1ccc89de7b87121c23cb78a14866
Forrest Evans <fdevans@gmail.com>
2024-06-13 13:35:32 -0700

    Announcement Samples

---
+++ docs/.vuepress/components/SidebarAnnounce.vue
@@ -0,0 +1,25 @@
+<template>
+    <AnnounceBox>
+      <template #top>
+        <div class="sidebar-announce">
+          <p>
+            <b>Process Automation On Prem</b><br> is being renamed to<br> <b>Runbook Automation - Self Hosted</b>
+          </p>
+    </div>
+      </template>
+    </AnnounceBox>
+</template>
+<script setup lang="ts">
+  import AnnounceBox from "vuepress-theme-hope/modules/sidebar/components/Sidebar.js";
+</script>
+
+<style>
+.sidebar-announce {
+    background-color: var(--theme-color-mask); /* Green color */
+    border-radius: 10px; /* Rounded corners */
+    padding: 1px 8px; /* Padding inside the box */
+    font-size: 14px; /* Small font size */
+    margin: 10px; /* outside padding */
+    text-align: center; /* Center the text */
+    }
+</style>
