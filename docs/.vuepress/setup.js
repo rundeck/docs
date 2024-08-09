8f44bf78391f19f1e4b871ed2e0cb614b26075c7
Forrest Evans <fdevans@gmail.com>
2024-08-09 10:11:45 -0700

    5.6.0 base updates

--- docs/.vuepress/setup.js
+++ docs/.vuepress/setup.js
@@ -1,7 +1,7 @@
 import { BaseTransition } from "vue"
 
-const RUNDECK_VERSION='5.5.0'
-const RUNDECK_VERSION_FULL='5.5.0-SNAPSHOT'
+const RUNDECK_VERSION='5.6.0'
+const RUNDECK_VERSION_FULL='5.6.0-SNAPSHOT'
 const API_VERSION='48'
 const API_DEP_REL='6.0.0'
 const API_DEP_VER='17'
