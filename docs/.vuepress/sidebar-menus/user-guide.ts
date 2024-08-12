d4af1081729f44c794d4e82c43bf0e228191629a
Forrest Evans <fdevans@gmail.com>
2024-08-12 08:24:51 -0700

    bring forward workflow-strategies

--- docs/.vuepress/sidebar-menus/user-guide.ts
+++ docs/.vuepress/sidebar-menus/user-guide.ts
@@ -96,7 +96,8 @@ export default [{
         '/manual/execution-lifecycle/result-data.md',
         '/manual/execution-lifecycle/roi-metrics.md',
         '/manual/jobs/job-queue.md',
-        '/manual/jobs/ai-generated-runbooks.md'
+        '/manual/jobs/ai-generated-runbooks.md',
+        '/manual/workflow-strategies'
       ]
     },
     {
