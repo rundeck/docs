0f83c14d31d8722c3233ab340010f1c776bdfaaf
Forrest Evans <fdevans@gmail.com>
2025-10-17 16:10:36 -0700

    Update setup.js

--- docs/.vuepress/setup.js
+++ docs/.vuepress/setup.js
@@ -2,7 +2,7 @@ import { BaseTransition } from "vue"
 
 const RUNDECK_VERSION='5.16.0'
 const RUNDECK_VERSION_FULL='5.16.0-SNAPSHOT'
-const API_VERSION='53'
+const API_VERSION='56'
 const API_DEP_REL='6.0.0'
 const API_DEP_VER='17'
 const API_MIN_VER='14'
